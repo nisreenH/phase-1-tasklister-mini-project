fa81f576945383064c23d36466772b31f351e19f
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-10-29 23:00:17 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  // let form = document.querySelector('form');
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    AddToDo(e.target.new_task_description.value)
+    form.reset()
+
+  })
 });
+
+function AddToDo(todo){
+  let li = document.createElement('li')
+  
+  let xbtn = document.createElement('button')
+  xbtn.addEventListener('click', handleDelete)
+  xbtn.textContent = 'x'
+
+  li.textContent = `${todo}`
+  li.appendChild(xbtn)
+
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
